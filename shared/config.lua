4d04d732cfc28177e3f0f67466b24af200ce5d02
Erazaya <130865353+Erazaya@users.noreply.github.com>
2023-09-23 22:58:11 +0200

    add config.lua

---
+++ shared/config.lua
@@ -0,0 +1,5 @@
+Config                  = {}
+
+Config.Lang = "en" ---Availables : en, fr / Default : en
+
+Config.ProgressBar = "circle" --circle for ox ProgressCircle (Recommended), ox for ox progressbar, qb for qb-progressbar

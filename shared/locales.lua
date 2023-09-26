000b519e7a593152c2c9cb5b6c47915607a7a47f
Erazaya <130865353+Erazaya@users.noreply.github.com>
2023-09-26 22:21:25 +0200

    french localization

--- shared/locales.lua
+++ shared/locales.lua
@@ -1,6 +1,8 @@
 -- /!\ WARNING : manual localizations will be required in qb-radialmenu config.lua and app.js of your inventory.
+--Any contributions on localizations are welcome !
 
-if Config.Lang == "en" then
+
+if Config.Lang == 'en' then
 
 Locales = {
     notif = {
@@ -21,22 +23,55 @@ Locales = {
     },
 
     progressbar = { --Only if you use progressbar
-        removing = "Removing...",
-        wearing = "Dressing up...",
+        removing = 'Removing...',
+        wearing = 'Dressing up...',
     },
 
     hair = {
-        saved = "Haircut saved",
-        nohaircut = "Save your haircut before switching",
+        saved = 'Haircut saved',
+        nohaircut = 'Save your haircut before switching',
 
-        manage = "Manage Haircuts",
-        save1 = "Save Hair 1",
-        save2 = "Save Hair 2",
-        actual = "Saved : ", --Dont remove the space at the end
+        manage = 'Manage Haircuts',
+        save1 = 'Save Hair 1',
+        save2 = 'Save Hair 2',
+        actual = 'Saved : ', --Dont remove the space at the end
 
     }
 }
 
-elseif Config.Lang == "fr" then
-
+elseif Config.Lang == 'fr' then
+    Locales = {
+        notif = {
+            done = 'Terminé !',
+            error = 'Erreur',
+            cancel = 'Annulé',
+            removed = 'Retiré !',
+            pederror = 'Ce n\'est pas accessible aux peds !',
+            pedprops = 'Ce n\'est pas accessible aux peds !',
+    
+            alremoved = 'Vous l\'avez déjà enlevé !',
+            notwear = 'Vous n\'en portez pas !',
+    
+            busy = 'Vous êtes déjà occupé !',
+    
+            donthave = 'Vous n\'avez pas',
+            onyou = 'sur vous',
+        },
+    
+        progressbar = { --Only if you use progressbar
+            removing = 'Déshabillage...',
+            wearing = 'Vous vous habillez...',
+        },
+    
+        hair = {
+            saved = 'Coupe sauvegardée',
+            nohaircut = 'Sauvegarder une coupe avant de de lchanger !',
+    
+            manage = 'Gérer vos cheveux',
+            save1 = 'Sauvegarder Coupe 1',
+            save2 = 'Sauvegarder Coupe 2',
+            actual = 'Actuelle : ', --Dont remove the space at the end
+    
+        }
+    }
 end
